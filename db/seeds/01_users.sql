5bad27044be1f8b0ba1a23a8f4151418076ab7d3
Eric Dokter <edokter11@gmail.com>
2022-04-27 13:06:00 -0600

    edit/insert users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email) VALUES ('Alice', 'alicethegreat@gmail.com');
-INSERT INTO users (name, email) VALUES ('Kira', 'krazykira@gmail.com');
-INSERT INTO users (name, email) VALUES ('Bruce', 'bigbadbruce@gmail.com')
+INSERT INTO users (name, email, role, is_user) VALUES ('Alice', 'alicethegreat@gmail.com', 'user', true);
+INSERT INTO users (name, email, role, is_user) VALUES ('Kira', 'krazykira@gmail.com', 'user', true);
+INSERT INTO users (name, email, role, is_user) VALUES ('Bruce', 'bigbadbruce@gmail.com', 'user', true);
+INSERT INTO users (name, email, role, is_user) VALUES ('Kevin', 'abc@gmail.com', 'admin', false);
+INSERT INTO users (name, email, role, is_user) VALUES ('Carol', 'bcd@gmail.com', 'admin', false);
