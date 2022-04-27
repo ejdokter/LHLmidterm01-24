631e498fa987bcc25d32fe476a356d8329313087
Eric Dokter <edokter11@gmail.com>
2022-04-27 11:20:12 -0600

    insert new users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email) VALUES ('Alice', 'alicethegreat@gmail.com');
+INSERT INTO users (name, email) VALUES ('Kira', 'krazykira@gmail.com');
+INSERT INTO users (name, email) VALUES ('Bruce', 'bigbadbruce@gmail.com')
