e1de327ce587e12854dca2951f808b1cc9fd6bdb
Eric Dokter <edokter11@gmail.com>
2022-04-28 12:12:56 -0600

    create main route

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
