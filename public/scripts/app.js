bbfe07c8cf0ced5625beaeb043e6a363ad9e48bb
Nathan Pare <nathanpare@Nathans-MacBook-Pro.local>
2022-04-30 13:22:25 -0700

    Fix conflict

---
+++ public/scripts/app.js
@@ -0,0 +1,29 @@
+
+// Client facing scripts here
+
+$(document).ready(function() {
+
+  const createProduct = function(productData) {
+
+    const $product = $(`<div class="product"></div>`)
+
+    const html = `
+
+      <div class="product-name">
+        <p>${productData.products.name}</p>
+      </div>
+
+      <div class="product-image">
+
+      </div>
+
+      <div class="description">
+        <p>description</p>
+        <p>${productData.products.description}</p>
+      </div>
+      <button class="button-24" role="button">Add to Cart</button>
+      </div>
+    `
+  }
+})
+
