589bf77703d4d68c9f03f5d2dc185f5602c96636
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-05-05 00:11:39 -0400

    cookie rendering working

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -2,15 +2,7 @@
 // Client facing scripts here
 
 
-// const createCartElement = function(cartData){
-//   // creating cart elements
-// }
-
 function setCookie(name, value) {
-  //   let pid =$(this).find("#pid").text();
-  //   let name =$(this).find("#pname").text();
-
-  //   console.log(pid, name, desc, catog, units);
 
   let cookie = name + "=" + encodeURIComponent(value);
   document.cookie = cookie;
@@ -28,33 +20,7 @@ function getCookie(name) {
   }
   return null;
 }
-// const loadCartData = function(){
-//    $.ajax({
-//       type: "POST",
-//       url: "/api/cart/add",
-//       dataType: 'json',
-//       data: obj,
-//       xhrFields: {
-//         withCredentials: true
-//       },
-//       crossDomain: true,
-//       cache: false,
-//       success: function (data) {
-//         alert(data);
-//       }
-//     }).then((response) => {
-//       console.log(response)
-//     });
 
-// }
-// const generateRandomNumber = function() {
-//   let result= ''
-//   let char = '0123456789';
-//   for (let i = 0; i < 8; i++) {
-//     result += char[Math.round(Math.random() * (char.length - 1))];
-//   }
-//   return parseInt(result);
-// };
 $(document).ready(function () {
   // On button click to add or delete create event handler
   // checkout event handler
@@ -64,30 +30,19 @@ $(document).ready(function () {
   });
 
   $(".add-button").click((e) => {
-    //$(document).on('click', '.buttonitem .add-button', function(e) {
+
     e.preventDefault();
     console.log("hello add", $(e.currentTarget).data());
 
-
     //const get cartdata variable
-    //console.log("product id", $(this).parents(".artcontent").children('.prodid'));
-    //console.log(this);
-    //console.log($(".artcontent"));
-    // let pid =$(".artcontent").not($(this).parents(".artcontent")).find("#pid").text();
-    // let name =$(".artcontent").not($(this).parents(".artcontent")).find("#pname").text();
-    //let pid = $(this).find("#pid").text();
-    //let name = $(this).find("#pname").text();
+
     let pid =$(e.currentTarget).data().id;
     let name =$(e.currentTarget).data().name
 
-    // let pid =$(this).closest(find("#pid")).text();
-    // let name =$(this).closest(find("#pname")).text();
 
     let objData = { id: pid, name: name };
     //save to cookie
 
-    //console.log(pid, name, desc, catog, units);
-    console.log(getCookie("cart"));
     let presentCookie = JSON.parse(getCookie("cart"));
     if (presentCookie) {
       presentCookie.push(objData);
@@ -97,15 +52,8 @@ $(document).ready(function () {
 
     }
     console.log({ presentCookie });
-    //console.log(JSON.stringify([]));
 
     setCookie("cart", JSON.stringify(presentCookie));
-
-    //cookie.clear();
-    // $.post('/add', serializedData, response =>{
-    //   console.log(response);
-    //   loadcart();
-    // })
 
   });
 
--- routes/cart.js
+++ routes/cart.js
@@ -2,21 +2,50 @@
 const express = require ('express');
 const router = express.Router();
 
+// function getCookie(name, req) {
+//   let nameK = name + "=";
+
+//   let manyNameK = req.cookies.split(';');
+//   for (var i = 0; i < manyNameK.length; i++) {
+//     var c = manyNameK[i];
+//     while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+//     if (c.indexOf(nameK) == 0) {
+//       return decodeURIComponent(c.substring(nameK.length, c.length));
+//     }
+//   }
+//   return null;
+// }
+
 module.exports = (db) => {
 
+
   router.get("/", (req, res) => {
+    //let presentCookie = JSON.parse(getCookie("cart", req));
+    let presentCookie =JSON.parse(req.cookies["cart"]);
+    //console.log(presentCookie);
+    let cookieIds =[];
+    for(cokie of presentCookie){
+      cookieIds.push(cokie.id);
+    }
+//console.log(cookieIds);
     const user = req.session.id
     if (!user) {
       return res.redirect('/api/login')
     }
-    let cart = req.cookies;
-    console.log(cart);
+   // let cart = req.cookies;
+    //console.log(cart);
     //const user1 = req.session.id
-    db.query(`SELECT * FROM products LIMIT 10;`)
+    db.query(`SELECT * FROM products;`)
     .then(data => {
      products = data.rows;
-     //console.log(products);
-     const templateVars = {user, products}
+     products = products.filter(product =>cookieIds.includes(product.id));
+
+     console.log(products);
+     let sum =0;
+     for(const prod of products){
+       sum+= prod.price;
+     }
+     const templateVars = {user, products, sum}
      res.render("cart", templateVars);
     })
     .catch(err => {
